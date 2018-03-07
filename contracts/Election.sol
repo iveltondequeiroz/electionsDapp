f88c1071970107233dc37d63767be2e314ee788e
iveltondequeiroz <iveltondequeiroz@gmail.com>
2018-03-06 21:54:01 -0400

    election contract

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,11 +1,26 @@
 pragma solidity ^0.4.2;
 
 contract Election {
-  // store candidate name
-  // read candidate
-  string public candidate;
+  // model a candidate
+  struct Candidate {
+    uint id;
+    string name;
+    uint voteCount;
+  }
+  // store candidates
+  // fetch candidate
+  mapping(uint => Candidate) public candidates;
+  // store candidates count
+  uint candidatesCount;
+
   // constructor
   function Election() public {
-    candidate = "Candidate 1";
+    addCandidate("Candidate 1");
+    addCandidate("Candidate 2");
+  }
+
+  function addCandidate(string _name) private {
+    candidatesCount++;
+    candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
   }
 }
